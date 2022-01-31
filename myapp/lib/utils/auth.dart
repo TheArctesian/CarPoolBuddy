43fdbf14ea5f47945914b8caf9d4983f113fd7c2
TheArctesian <stephen.d.okita@gmail.com>
2022-01-31 11:48:43 +0800

    fixed some bugs

--- myapp/lib/utils/auth.dart
+++ myapp/lib/utils/auth.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/foundation.dart' show kIsWeb;
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:flutter/material.dart';
+
 
 class Authentication {
   static Future<User?> signInWithGoogle({required BuildContext context}) async {
